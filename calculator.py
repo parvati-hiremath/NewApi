496a7a3fdb6e0532d32a19ed5c1f62d6621bd8b1
parvati-hiremath <parvatihiremath2024@outlook.com>
2025-11-12 15:53:57 +0530

    Refactor calculator functions and fix input/output
    Fix function definitions and implement multiplication and addition correctly. Update user input handling and output formatting.

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def multiplication(a, b)
+  return(a*b)
+
+def addition(f, g)
+  return(f+b)
+
+#get input from user
+x = int(input("enter a : "))
+y = int(input("enter b : "))
+print(f"{a} + {b} = {addition(a,b)}")
+print(f"{a} + {b} = {substraction(a,b)}")
--- claculator.py
+++
@@ -1,7 +0,0 @@
-def multiplication(2.3)
-a= 2*3
-print(a)
-
-def addition(4,4)
-b= 4+4
-print(b)
